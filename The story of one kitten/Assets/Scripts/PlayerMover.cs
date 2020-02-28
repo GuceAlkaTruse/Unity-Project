fb7233e45515cdbb710072a82895b3a8ceca50e7
GuceAlkaTruse <60811855+GuceAlkaTruse@users.noreply.github.com>
2020-02-28 21:42:02 +0200

    first commit

---
+++ The story of one kitten/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            _animator.SetInteger("TransitionCheck", 2);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            _animator.SetInteger("TransitionCheck", 2);
+        }
+        else
+        {
+            _animator.SetInteger("TransitionCheck", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpForce);
+        }
+    }
+}
---
+++ The story of one kitten/Assets/Scripts/SpawnHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHealth : MonoBehaviour
+{
+    [SerializeField] private GameObject coins;
+
+    private float randomX;
+    private Vector2 whereToSpawn;
+
+     void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Coin")
+        {
+            StartCoroutine(RespawnCoin(collider.gameObject));
+        }
+    }
+    private IEnumerator RespawnCoin(GameObject obj)
+    {
+        randomX = Random.Range(-10f, 10f);
+        whereToSpawn = new Vector2(randomX, transform.position.y);
+        Instantiate(coins, whereToSpawn, Quaternion.identity);
+        Destroy(obj);
+        yield return new WaitForSeconds(2f);
+    }
+}
